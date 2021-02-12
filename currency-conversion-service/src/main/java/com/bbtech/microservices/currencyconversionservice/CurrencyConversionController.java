3293a49a86df490f3a86c8400d480bbab1fe000d
Bhabadyuti <bbal@sii.pl>
2021-02-12 13:02:21 +0530

    organized imports

--- currency-conversion-service/src/main/java/com/bbtech/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/bbtech/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,13 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.net.ConnectException;
 
 @Slf4j
 @RestController
