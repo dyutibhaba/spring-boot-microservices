19ef465316f9e0d1ef7d736bf7733c3cb1faccfc
Bhabadyuti <bbal@sii.pl>
2021-02-17 17:55:47 +0530

    added distributed tracing dependencies

--- currency-conversion-service/src/main/java/com/bbtech/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/bbtech/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //application name of the microservice to be called. Check properties file
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+/*@FeignClient(name = "currency-exchange", url = "localhost:8000")*/
+//to act as load balancer remove url
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
