0ca114ca828c35dc8c328b6daf0adcbe2b8712b9
Bhabadyuti <bbal@sii.pl>
2021-02-17 17:56:33 +0530

    Distributed Tracing config

--- currency-exchange-service/src/main/java/com/bbtech/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/bbtech/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,6 @@
 package com.bbtech.microservices.currencyexchangeservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
-
+@Slf4j
 @RestController
 @RequestMapping(value = "/currency-exchange")
 public class CurrencyExchangeController {
@@ -25,6 +26,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/from/{from}/to/{to}")
     public CurrencyExchange retrieveCurrencyExchange(@PathVariable String from, @PathVariable String to) {
+        log.info("retrieveCurrencyExchange called with {} to {}", from, to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
         if(null == currencyExchange) {
